4ccf3edba9689830971b2b1eca8fff6ac01e64e4
Steve Scherrer <stevescherrer@users.noreply.github.com>
2020-04-22 13:13:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SF Commuter housing
+ A tool to visualize commute times for users looking for housing in San Francisco
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to visualize neighborhoods in San Francisco based on the the transit time to aa fixed reference point.',
+    author='Steve Scherrer',
+    license='MIT',
+)
